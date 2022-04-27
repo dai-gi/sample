ed85c02a3204c806d0a4ea74ed44d6ebd81f88d8
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-04-27 20:30:44 +0900

    Test: 試しでマイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_23_045256) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_27_112604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
